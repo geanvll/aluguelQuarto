641292153be86b439e2c7276f6bfb6e37f646f00
Gean Lopes <geanvll@outlook.com>
2023-11-09 20:34:04 -0300

    Comments

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,10 +9,14 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		//Programa que simula uma pensão para estudantes, onde 15 quartos poderão ser alugados.
+		//Temos um vetor 'hotel' que guardará os dados dos alunos que quiseram alugar.
+		//E no fim, mostra os quartos alugados em ordem, junto com os dados dos clientes.
+		
 		Locale.setDefault(Locale.US);
 		Scanner t = new Scanner(System.in);
 		
-		Aluguel [] hotel = new Aluguel[10];
+		Aluguel [] hotel = new Aluguel[15];
 		
 		System.out.print("Quantos quartos serão alugados ? ");
 		int n = t.nextInt();
@@ -24,14 +28,16 @@ public class Program {
 			String nome = t.nextLine();
 			System.out.print("E-mail: ");
 			String email = t.nextLine();
-			System.out.println("Quarto: ");
+			System.out.print("Quarto: ");
 			int quarto = t.nextInt();
 			
-			hotel[quarto] = new Aluguel(nome, email);
+			Aluguel aluguel = new Aluguel(nome, email);
+			
+			hotel[quarto] = aluguel;
 		}
 		
 		System.out.println("Quartos ocupados: ");
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 15; i++) {
 			if(hotel[i] != null) {
 				System.out.println(i + ": "+ hotel[i]);
 			}
